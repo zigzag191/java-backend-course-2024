cab5f53a38e3f368b03ba5b472073c5ce27bcdde
zigzag <wwe.15@list.ru>
2024-03-31 18:52:20 +0300

    annotate jpa repositories and enable scheduler

--- scrapper/src/main/java/edu/java/scrapper/repository/jpa/JpaLinkRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/repository/jpa/JpaLinkRepository.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaLinkRepository extends JpaRepository<LinkEntity, Long> {
 
     Optional<LinkEntity> findByUrl(URI url);
